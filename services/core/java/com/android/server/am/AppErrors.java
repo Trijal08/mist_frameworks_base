c2828c582b7ad0cf039c519ef41c144b1502c652
Dil3mm4 <dil3mm4.dev@gmail.com>
2019-02-12 13:52:32 +0000

    core: Add product board in the log
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: Ie964d1d9a4e73111bf2569f522fc06a52454c044
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- services/core/java/com/android/server/am/AppErrors.java
+++ services/core/java/com/android/server/am/AppErrors.java
@@ -680,11 +680,13 @@ class AppErrors {
             }
 
             // Add paste content for Memochō option
-            String devfp = SystemProperties.get("ro.vendor.build.fingerprint", "");
-            String everestVers = SystemProperties.get("ro.everest.version", "");
+            String devFP = SystemProperties.get("ro.vendor.build.fingerprint", "");
+            String mistVers = SystemProperties.get("ro.mist.version", "");
+            String boardName = SystemProperties.get("ro.product.board", "");
             data.paste = "time: " + timeMillis + "\n" +
-            "device fp:" + devfp + "\n" +
-            "everest vers:" + everestVers + "\n" +
+            "device fp:" + devFP + "\n" +
+            "product board:" + boardName + "\n" +
+            "mistos vers:" + mistVers + "\n" +
             "msg: " + longMsg + "\n" +
             "stacktrace: " + stackTrace;
 
