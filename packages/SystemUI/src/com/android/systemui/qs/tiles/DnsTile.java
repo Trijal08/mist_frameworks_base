a27082a88903a38d8568e465903a5c7206d85151
Zabuka_zuzu <zuzu950444@gmail.com>
2026-05-07 19:38:08 +0000

    SystemUI: fixup Add private DNS QS tile

--- packages/SystemUI/src/com/android/systemui/qs/tiles/DnsTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/DnsTile.java
@@ -149,7 +149,7 @@ public class DnsTile extends QSTileImpl<BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.LUNARIS;
+        return MetricsEvent.MIST;
     }
 
     private String getSecondaryLabel(int mode) {
