412219933150afa6379385efaa0e830f6adb4f1f
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2026-05-27 19:44:13 -0400

    Revert "PixelPropsUtils: Move from props to Settings switch"
    This reverts commit 8ad80845d04a6377f0a51f5f4e55cc08f99bf270.

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -795,8 +795,8 @@ public class ApplicationPackageManager extends PackageManager {
             final List<FeatureInfo> list = new ArrayList<>(parceledList.getList());
 
             // Inject Tensor features when toggle is enabled
-            final boolean forceTensor = !Process.isIsolated() && Settings.Secure.getInt(
-                    mContext.getContentResolver(), Settings.Secure.PI_TENSOR_SPOOF, 0) == 1;
+            final boolean forceTensor = !Process.isIsolated() &&
+                    SystemProperties.getBoolean("persist.sys.pp.tensor", false);
 
             if (forceTensor && !IS_TENSOR_DEVICE) {
                 for (String feature : FEATURES_TENSOR) {
@@ -948,8 +948,7 @@ public class ApplicationPackageManager extends PackageManager {
         if (name != null && pkg != null && PRIV_PKGS.contains(pkg)) {
             final boolean photosSpoof = !Process.isIsolated()
                 && "com.google.android.apps.photos".equals(pkg)
-                && (Settings.Secure.getInt(mContext.getContentResolver(),
-                Settings.Secure.PI_PHOTOS_SPOOF, 1) == 1);
+                && SystemProperties.getBoolean("persist.sys.pp.photos", true);
             if (photosSpoof) {
                 if (FEATURES_PIXEL.contains(name)) return false;
                 if (FEATURES_PIXEL_OTHERS.contains(name)) return true;
@@ -964,8 +963,8 @@ public class ApplicationPackageManager extends PackageManager {
         }
 
         if (name != null && FEATURES_TENSOR.contains(name)) {
-            final boolean forceTensor = !Process.isIsolated() && Settings.Secure.getInt(
-                    mContext.getContentResolver(), Settings.Secure.PI_TENSOR_SPOOF, 0) == 1;
+            final boolean forceTensor = !Process.isIsolated() &&
+                    SystemProperties.getBoolean("persist.sys.pp.tensor", false);
 
             // Do not interfere with real Tensor devices
             if (IS_TENSOR_DEVICE) {
