drkphnx/android_frameworks_base

Add support for UDFPS icons
* This is complete rewrite basis UDFPS refactor. Cleaner and without hacks.

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/biometrics/UdfpsIconDrawable.java
@@ -1,0 +1,138 @@
+/*
+ * Copyright (C) 2024 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.biometrics;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.ColorFilter;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.provider.Settings;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
+
+import com.android.internal.util.crdroid.Utils;
+import com.android.systemui.Dependency;
+import com.android.systemui.res.R;
+import com.android.systemui.tuner.TunerService;
+
+/**
+ * Abstract base class for drawable displayed when the finger is not touching the
+ * sensor area.
+ */
+public abstract class UdfpsIconDrawable extends Drawable {
+
+    private static final String UDFPS_ICON = "system:" + Settings.System.UDFPS_ICON;
+    private final String udfpsResourcesPackage = "com.crdroid.udfps.icons";
+
+    @NonNull private final Context mContext;
+    private Drawable mUdfpsDrawable;
+    private Resources udfpsRes;
+    private String[] mUdfpsIcons;
+
+    private final TunerService.Tunable mTunable = (key, newValue) -> {
+        if (UDFPS_ICON.equals(key)) {
+            int selectedIcon = newValue == null ? 0 : Integer.parseInt(newValue);
+            if (mUdfpsDrawable != null) {
+                mUdfpsDrawable.setCallback(null);  // Clear previous drawable callback to avoid leaks
+                mUdfpsDrawable = null;
+            }
+            mUdfpsDrawable = selectedIcon == 0 ? null : loadDrawable(udfpsRes, mUdfpsIcons[selectedIcon]);
+        }
+    };
+
+    public UdfpsIconDrawable(@NonNull Context context) {
+        mContext = context;
+        init();
+    }
+
+    private void init() {
+        if (Utils.isPackageInstalled(mContext, udfpsResourcesPackage)) {
+            try {
+                PackageManager pm = mContext.getPackageManager();
+                udfpsRes = pm.getResourcesForApplication(udfpsResourcesPackage);
+                int res = udfpsRes.getIdentifier("udfps_icons", "array", udfpsResourcesPackage);
+                mUdfpsIcons = udfpsRes.getStringArray(res);
+                Dependency.get(TunerService.class).addTunable(mTunable, UDFPS_ICON);
+            } catch (PackageManager.NameNotFoundException e) {
+                Log.e("UdfpsIconDrawable", "UDFPS package not found", e);
+            }
+        }
+    }
+
+    public void destroy() {
+        Dependency.get(TunerService.class).removeTunable(mTunable);
+    }
+
+    @Override
+    public void setAlpha(int alpha) {
+        if (mUdfpsDrawable != null) {
+            mUdfpsDrawable.setAlpha(alpha);
+        }
+        invalidateSelf();
+    }
+
+    Drawable getUdfpsDrawable() {
+        return mUdfpsDrawable;
+    }
+
+    private Drawable loadDrawable(Resources res, String resName) {
+        if (res == null || resName == null) {
+            return null;
+        }
+        int resId = res.getIdentifier(resName, "drawable", udfpsResourcesPackage);
+        return resId != 0 ? ResourcesCompat.getDrawable(res, resId, null) : null;
+    }
+
+    @Override
+    public void setBounds(int left, int top, int right, int bottom) {
+        if (mUdfpsDrawable != null) {
+            mUdfpsDrawable.setBounds(left , top, right, bottom);
+        }
+        invalidateSelf();
+    }
+
+    @Override
+    public void setBounds(Rect bounds) {
+        if (mUdfpsDrawable != null) {
+            mUdfpsDrawable.setBounds(bounds);
+        }
+        invalidateSelf();
+    }
+
+    @Override
+    protected void onBoundsChange(Rect bounds) {
+        if (mUdfpsDrawable != null) {
+            mUdfpsDrawable.setBounds(bounds);
+        }
+        invalidateSelf();
+    }
+
+    @Override
+    public void setColorFilter(@Nullable ColorFilter colorFilter) {
+        // Do not set color filter
+    }
+
+    @Override
+    public int getOpacity() {
+        return 0;
+    }
+}

packages/SystemUI/src/com/android/systemui/keyguard/ui/view/DeviceEntryIconView.kt
@@ -19,6 +19,8 @@ package com.android.systemui.keyguard.ui.view
 import android.content.Context
 import android.graphics.drawable.AnimatedStateListDrawable
 import android.graphics.drawable.AnimatedVectorDrawable
+import android.os.UserHandle
+import android.provider.Settings
 import android.util.AttributeSet
 import android.util.StateSet
 import android.view.Gravity
@@ -30,8 +32,12 @@ import android.widget.ImageView
 import androidx.core.view.accessibility.AccessibilityNodeInfoCompat
 import com.airbnb.lottie.LottieCompositionFactory
 import com.airbnb.lottie.LottieDrawable
+import com.android.systemui.Dependency
+import com.android.systemui.biometrics.UdfpsFpIconDrawable
+import com.android.systemui.biometrics.UdfpsIconDrawable
 import com.android.systemui.common.ui.view.LongPressHandlingView
 import com.android.systemui.res.R
+import com.android.systemui.tuner.TunerService
 
 class DeviceEntryIconView
 @JvmOverloads
@@ -39,7 +45,8 @@ constructor(
     context: Context,
     attrs: AttributeSet?,
     defStyleAttrs: Int = 0,
-) : FrameLayout(context, attrs, defStyleAttrs) {
+    tunerService: TunerService,
+) : FrameLayout(context, attrs, defStyleAttrs), TunerService.Tunable {
     val longPressHandlingView: LongPressHandlingView = LongPressHandlingView(context, attrs)
     val iconView: ImageView = ImageView(context, attrs).apply { id = R.id.device_entry_icon_fg }
     val bgView: ImageView = ImageView(context, attrs).apply { id = R.id.device_entry_icon_bg }
@@ -47,6 +54,15 @@ constructor(
     var accessibilityHintType: AccessibilityHintType = AccessibilityHintType.NONE
 
     private var animatedIconDrawable: AnimatedStateListDrawable = AnimatedStateListDrawable()
+
+    private final val UDFPS_ICON: String =
+            "system:" + Settings.System.UDFPS_ICON
+
+    private val fingerprintDrawable: UdfpsIconDrawable = UdfpsFpIconDrawable(context)
+
+    private val packageInstalled = com.android.internal.util.crdroid.Utils.isPackageInstalled(
+        context, "com.crdroid.udfps.icons"
+    )
 
     init {
         setupIconStates()
@@ -58,6 +74,21 @@ constructor(
         addBgImageView()
         addIconImageView()
         addLongpressHandlingView()
+
+        tunerService.addTunable(this, UDFPS_ICON)
+    }
+
+    override fun onTuningChanged(key: String?, value: String?) {
+        when (key) {
+            UDFPS_ICON -> {
+                iconView.setImageDrawable(null)
+                animatedIconDrawable = AnimatedStateListDrawable()
+                setupIconStates()
+                setupIconTransitions()
+                iconView.setImageDrawable(animatedIconDrawable) 
+            }
+            else -> return
+        }
     }
 
     private fun setupAccessibilityDelegate() {
@@ -95,6 +126,10 @@ constructor(
      * - Drawable properties can be updated using ImageView properties like imageTintList.
      */
     private fun setupIconStates() {
+        val customUdfpsIcon = packageInstalled && (Settings.System.getIntForUser(
+            mContext.contentResolver, Settings.System.UDFPS_ICON, 0, UserHandle.USER_CURRENT
+        ) != 0)
+
         // Lockscreen States
         // LOCK
         animatedIconDrawable.addState(
@@ -109,11 +144,20 @@ constructor(
             R.id.unlocked,
         )
         // FINGERPRINT
-        animatedIconDrawable.addState(
-            getIconState(IconType.FINGERPRINT, false),
-            context.getDrawable(R.drawable.ic_fingerprint)!!,
-            R.id.locked_fp,
-        )
+        if (customUdfpsIcon) {
+            fingerprintDrawable.setBounds(0, 0, bgView.width, bgView.height)
+            animatedIconDrawable.addState(
+                getIconState(IconType.FINGERPRINT, false),
+                fingerprintDrawable,
+                R.id.locked_fp
+            )
+        } else {
+            animatedIconDrawable.addState(
+                getIconState(IconType.FINGERPRINT, false),
+                context.getDrawable(R.drawable.ic_fingerprint)!!,
+                R.id.locked_fp
+            )
+        }
 
         // AOD states
         // LOCK

Trijal08/mist_frameworks_base

Change-Id: If3e1fb82d3ecc259a5395ca97e44263197855b72

packages/SystemUI/src/com/android/systemui/biometrics/UdfpsIconDrawable.java
@@ -1,0 +1,138 @@
+/*
+ * Copyright (C) 2024-2025 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.biometrics;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.ColorFilter;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.provider.Settings;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
+
+import com.android.internal.util.mist.Utils;
+import com.android.systemui.Dependency;
+import com.android.systemui.res.R;
+import com.android.systemui.tuner.TunerService;
+
+/**
+ * Abstract base class for drawable displayed when the finger is not touching the
+ * sensor area.
+ */
+public abstract class UdfpsIconDrawable extends Drawable {
+
+    private static final String UDFPS_ICON = "system:" + Settings.System.UDFPS_ICON;
+    private final String udfpsResourcesPackage = "com.mist.udfps.icons";
+
+    @NonNull private final Context mContext;
+    private Drawable mUdfpsDrawable;
+    private Resources udfpsRes;
+    private String[] mUdfpsIcons;
+
+    private final TunerService.Tunable mTunable = (key, newValue) -> {
+        if (UDFPS_ICON.equals(key)) {
+            int selectedIcon = newValue == null ? 0 : Integer.parseInt(newValue);
+            if (mUdfpsDrawable != null) {
+                mUdfpsDrawable.setCallback(null);  // Clear previous drawable callback to avoid leaks
+                mUdfpsDrawable = null;
+            }
+            mUdfpsDrawable = selectedIcon == 0 ? null : loadDrawable(udfpsRes, mUdfpsIcons[selectedIcon]);
+        }
+    };
+
+    public UdfpsIconDrawable(@NonNull Context context) {
+        mContext = context;
+        init();
+    }
+
+    private void init() {
+        if (Utils.isPackageInstalled(mContext, udfpsResourcesPackage)) {
+            try {
+                PackageManager pm = mContext.getPackageManager();
+                udfpsRes = pm.getResourcesForApplication(udfpsResourcesPackage);
+                int res = udfpsRes.getIdentifier("udfps_icons", "array", udfpsResourcesPackage);
+                mUdfpsIcons = udfpsRes.getStringArray(res);
+                Dependency.get(TunerService.class).addTunable(mTunable, UDFPS_ICON);
+            } catch (PackageManager.NameNotFoundException e) {
+                Log.e("UdfpsIconDrawable", "UDFPS package not found", e);
+            }
+        }
+    }
+
+    public void destroy() {
+        Dependency.get(TunerService.class).removeTunable(mTunable);
+    }
+
+    @Override
+    public void setAlpha(int alpha) {
+        if (mUdfpsDrawable != null) {
+            mUdfpsDrawable.setAlpha(alpha);
+        }
+        invalidateSelf();
+    }
+
+    Drawable getUdfpsDrawable() {
+        return mUdfpsDrawable;
+    }
+
+    private Drawable loadDrawable(Resources res, String resName) {
+        if (res == null || resName == null) {
+            return null;
+        }
+        int resId = res.getIdentifier(resName, "drawable", udfpsResourcesPackage);
+        return resId != 0 ? ResourcesCompat.getDrawable(res, resId, null) : null;
+    }
+
+    @Override
+    public void setBounds(int left, int top, int right, int bottom) {
+        if (mUdfpsDrawable != null) {
+            mUdfpsDrawable.setBounds(left , top, right, bottom);
+        }
+        invalidateSelf();
+    }
+
+    @Override
+    public void setBounds(Rect bounds) {
+        if (mUdfpsDrawable != null) {
+            mUdfpsDrawable.setBounds(bounds);
+        }
+        invalidateSelf();
+    }
+
+    @Override
+    protected void onBoundsChange(Rect bounds) {
+        if (mUdfpsDrawable != null) {
+            mUdfpsDrawable.setBounds(bounds);
+        }
+        invalidateSelf();
+    }
+
+    @Override
+    public void setColorFilter(@Nullable ColorFilter colorFilter) {
+        // Do not set color filter
+    }
+
+    @Override
+    public int getOpacity() {
+        return 0;
+    }
+}

packages/SystemUI/src/com/android/systemui/keyguard/ui/view/DeviceEntryIconView.kt
@@ -20,6 +20,8 @@ import android.companion.virtualdevice.flags.Flags
 import android.content.Context
 import android.graphics.drawable.AnimatedStateListDrawable
 import android.graphics.drawable.AnimatedVectorDrawable
+import android.os.UserHandle
+import android.provider.Settings
 import android.util.AttributeSet
 import android.util.StateSet
 import android.view.Gravity
@@ -32,9 +34,13 @@ import android.widget.ImageView
 import androidx.core.view.accessibility.AccessibilityNodeInfoCompat
 import com.airbnb.lottie.LottieCompositionFactory
 import com.airbnb.lottie.LottieDrawable
+import com.android.systemui.Dependency
+import com.android.systemui.biometrics.UdfpsFpIconDrawable
+import com.android.systemui.biometrics.UdfpsIconDrawable
 import com.android.systemui.common.ui.view.TouchHandlingView
 import com.android.systemui.log.TouchHandlingViewLogger
 import com.android.systemui.res.R
+import com.android.systemui.tuner.TunerService
 
 class DeviceEntryIconView
 @JvmOverloads
@@ -43,7 +49,8 @@ constructor(
     attrs: AttributeSet?,
     defStyleAttrs: Int = 0,
     logger: TouchHandlingViewLogger? = null,
-) : FrameLayout(context, attrs, defStyleAttrs) {
+    tunerService: TunerService,
+) : FrameLayout(context, attrs, defStyleAttrs), TunerService.Tunable {
 
     val touchHandlingView: TouchHandlingView =
         TouchHandlingView(
@@ -64,6 +71,15 @@ constructor(
 
     private var animatedIconDrawable: AnimatedStateListDrawable = AnimatedStateListDrawable()
 
+    private final val UDFPS_ICON: String =
+            "system:" + Settings.System.UDFPS_ICON
+
+    private val fingerprintDrawable: UdfpsIconDrawable = UdfpsFpIconDrawable(context)
+
+    private val packageInstalled = com.android.internal.util.mist.Utils.isPackageInstalled(
+        context, "com.mist.udfps.icons"
+    )
+
     init {
         setupIconStates()
         setupIconTransitions()
@@ -74,6 +90,21 @@ constructor(
         addBgImageView()
         addIconImageView()
         addTouchHandlingView()
+
+        tunerService.addTunable(this, UDFPS_ICON)
+    }
+
+    override fun onTuningChanged(key: String?, value: String?) {
+        when (key) {
+            UDFPS_ICON -> {
+                iconView.setImageDrawable(null)
+                animatedIconDrawable = AnimatedStateListDrawable()
+                setupIconStates()
+                setupIconTransitions()
+                iconView.setImageDrawable(animatedIconDrawable) 
+            }
+            else -> return
+        }
     }
 
     private fun setupAccessibilityDelegate() {
@@ -111,6 +142,10 @@ constructor(
      * - Drawable properties can be updated using ImageView properties like imageTintList.
      */
     private fun setupIconStates() {
+        val customUdfpsIcon = packageInstalled && (Settings.System.getIntForUser(
+            mContext.contentResolver, Settings.System.UDFPS_ICON, 0, UserHandle.USER_CURRENT
+        ) != 0)
+
         // Lockscreen States
         // LOCK
         animatedIconDrawable.addState(
@@ -128,11 +163,20 @@ constructor(
         LottieCompositionFactory.fromRawRes(mContext, R.raw.udfps_lockscreen_fp).addListener { result ->
             aodFpDrawable.setComposition(result)
         }
-        animatedIconDrawable.addState(
-            getIconState(IconType.FINGERPRINT, false),
-            aodFpDrawable,
-            R.id.locked_fp,
-        )
+        if (customUdfpsIcon) {
+            fingerprintDrawable.setBounds(0, 0, bgView.width, bgView.height)
+            animatedIconDrawable.addState(
+                getIconState(IconType.FINGERPRINT, false),
+                fingerprintDrawable,
+                R.id.locked_fp
+            )
+        } else {
+            animatedIconDrawable.addState(
+                getIconState(IconType.FINGERPRINT, false),
+                aodFpDrawable,
+                R.id.locked_fp,
+            )
+        }
 
         // AOD states
         // LOCK
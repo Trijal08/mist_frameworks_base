8a738f48ba9f362ea0405c4d7523e095e2c860ef
Pranav Vashi <neobuddy89@gmail.com>
2024-11-11 05:49:04 +0530

    Add support for UDFPS icons
    * This is complete rewrite basis UDFPS refactor. Cleaner and without hacks.
    
    Change-Id: If3e1fb82d3ecc259a5395ca97e44263197855b72
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7102,6 +7102,11 @@ public final class Settings {
          * @hide
          */
         public static final String UDFPS_ANIM_OFFSET = "udfps_anim_offset";
+        
+        /**
+         * @hide
+         */
+        public static final String UDFPS_ICON = "udfps_icon";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
