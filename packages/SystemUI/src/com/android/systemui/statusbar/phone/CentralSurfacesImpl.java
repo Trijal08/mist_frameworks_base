PisselShit/frameworks_base1

SystemUI: Implement device idle manager [1/2]
- Introduce a per-app idle management system that enforces background
process restrictions when the screen is off

- The screen-off/on integration points in CentralSurfacesImpl are
inspired by Descendant OS

Signed-off-by: Ghosuto <clash.raja10@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -110,6 +110,7 @@ import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.KeyguardUpdateMonitorCallback;
 import com.android.keyguard.ViewMediatorCallback;
 import com.android.systemui.ActivityIntentHelper;
+import com.android.systemui.lunaris.LunarisIdleManager;
 import com.android.systemui.AutoReinflateContainer;
 import com.android.systemui.CoreStartable;
 import com.android.systemui.DejankUtils;
@@ -2731,6 +2732,14 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
             }
 
             DejankUtils.stopDetectingBlockingIpcs(tag);
+            if (Settings.Secure.getInt(mContext.getContentResolver(),
+                    Settings.Secure.IDLE_MANAGER, 1) == 1) {
+                LunarisIdleManager.initManager(mContext);
+                LunarisIdleManager mgr = LunarisIdleManager.getInstance();
+                if (mgr != null && !mgr.isRunning()) {
+                    mgr.executeManager();
+                }
+            }
         }
 
         @Override
@@ -2755,6 +2764,10 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
             });
             DejankUtils.stopDetectingBlockingIpcs(tag);
             mHandler.removeCallbacks(mSystemUiGcOpt);
+            LunarisIdleManager mgr = LunarisIdleManager.getInstance();
+            if (mgr != null) {
+                mgr.haltManager();
+            }
         }
 
         /**

Trijal08/mist_frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -110,6 +110,7 @@ import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.KeyguardUpdateMonitorCallback;
 import com.android.keyguard.ViewMediatorCallback;
 import com.android.systemui.ActivityIntentHelper;
+import com.android.systemui.mist.MistIdleManager;
 import com.android.systemui.AutoReinflateContainer;
 import com.android.systemui.CoreStartable;
 import com.android.systemui.DejankUtils;
@@ -2720,6 +2721,14 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
             }
 
             DejankUtils.stopDetectingBlockingIpcs(tag);
+            if (Settings.Secure.getInt(mContext.getContentResolver(),
+                    Settings.Secure.IDLE_MANAGER, 1) == 1) {
+                MistIdleManager.initManager(mContext);
+                MistIdleManager mgr = MistIdleManager.getInstance();
+                if (mgr != null && !mgr.isRunning()) {
+                    mgr.executeManager();
+                }
+            }
         }
 
         @Override
@@ -2744,6 +2753,10 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
             });
             DejankUtils.stopDetectingBlockingIpcs(tag);
             mHandler.removeCallbacks(mSystemUiGcOpt);
+            MistIdleManager mgr = MistIdleManager.getInstance();
+            if (mgr != null) {
+                mgr.haltManager();
+            }
         }
 
         /**